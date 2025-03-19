c450ab2dcf71965fff56d2fc2590a271d9cba917
IsaqueRaFiPetri <isq.rafipe@gmail.com>
2025-03-19 14:38:55 -0300

    Save e Load Funcionando

--- CyberSecurity_Bruno/Assets/Scripts/SaveGame.cs
+++ CyberSecurity_Bruno/Assets/Scripts/SaveGame.cs
@@ -14,6 +14,30 @@ public class SaveGame : MonoBehaviour
     {
         savePath = Application.persistentDataPath + "/savegame.json";
     }
+
+    public void ToSaveGame()
+    {
+        string json = JsonUtility.ToJson(playerData, prettyPrint: true);
+        File.WriteAllText(savePath, json);
+        Debug.Log("Jogo salvo em: " + savePath);
+    }
+    public void ToLoadGame()
+    {
+        if (File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            playerData = JsonUtility.FromJson<PlayerData>(json);
+            Debug.Log("Jogo carregado");
+            texto.text = playerData.ToString();
+            return;
+        }
+        else
+        {
+            Debug.Log("Arquivo de save não encontrado.");
+            return;
+        }
+    }
+
 }
 
 [System.Serializable]
@@ -25,14 +49,14 @@ public class PlayerData
     public string[] inventario;
     string invTostring;
 
-    /*public override string ToString()
+    public override string ToString()
     {
         foreach (var item in inventario)
         {
             invTostring += item.ToString() + " ";
         }
-        return $@ {this.nome} {this.level}
+        return $@" {this.nome} {this.level}
         {this.vida} 
-        { this.invTostring};
-    }*/
+        { this.invTostring}";
+    }
 }
