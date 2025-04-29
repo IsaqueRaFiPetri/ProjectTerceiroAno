e93af7cf5f7800b8d1c10f41fb3968c8d5978f67
IsaqueRaFiPetri <isq.rafipe@gmail.com>
2025-04-29 15:29:32 -0300

    ...

--- OnlineTerceirao/Assets/Scripts/ConnectionController.cs
+++ OnlineTerceirao/Assets/Scripts/ConnectionController.cs
@@ -1,17 +1,43 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class ConnectionController : MonoBehaviourPunCallbacks
+public class ConnectionController : MonoBehaviourPunCallbacks //tudo visto na aula do professor alexandre
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnected()
     {
-        
+        print("Connected to the Internet!");
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to Photon's Server.");
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        print("Connected to the Lobby!");
+
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+    public override void OnJoinedRoom()
+    {
+        print("Acess the Room.");
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        print("Fail to join random room!");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        print("room created!");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        print("Failed to join room.");
     }
 }
